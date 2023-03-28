f529d668d882685051498329ab5cb544e9025115
Rodion Belovitskiy <belovickiy.rodion@gmail.com>
2023-03-28 23:24:12 +0300

    refactor: cleanup project

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,1 @@
-#[macro_use]
-extern crate rocket;
-
-use rocket::{Build, Rocket};
-
-mod routes;
-
-#[launch]
-pub(crate) fn rocket() -> Rocket<Build> {
-    rocket::build().mount("/", routes![routes::home::get_home])
-}
+fn main() {}
