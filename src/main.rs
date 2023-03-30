7c8a2c5146d3d2fc38f21a5643561dd0aa53e9f3
Rodion Belovitskiy <belovickiy.rodion@gmail.com>
2023-03-30 23:56:21 +0300

    feat: add actix-web

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,19 @@
-fn main() {}
+use actix_web::{get, HttpResponse, Responder};
+use env_logger::Env;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    use actix_web::{middleware::Logger, App, HttpServer};
+
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+    HttpServer::new(|| App::new().service(root).wrap(Logger::default()))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
+#[get("/")]
+async fn root() -> impl Responder {
+    HttpResponse::Ok().body("Root endpoint")
+}
