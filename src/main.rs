c17b2dec7dfe43371701e1aa5b3db77419e61832
Rodion Belovitskiy <belovickiy.rodion@gmail.com>
2023-04-15 23:30:41 +0300

    feat(actix): test get/post requests

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,54 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, get, App, HttpResponse, HttpServer, Responder, Result, Error};
+use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Clone)]
+struct Character {
+    id: u32,
+    name: String,
+}
+
+struct Repository {
+    characters: HashMap<u32, Character>,
+}
+
+impl Repository {
+    fn new() -> Self {
+        Self {
+            characters: HashMap::new(),
+        }
+    }
+}
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn echo() -> impl Responder {
+    HttpResponse::Ok().body("The server is working correctly!")
+}
+
+async fn create_character(character: web::Json<Character>, data: web::Data<Repository>) -> Result<String> { 
+    Ok(format!("New character: {}!", character.name))
+}
+
+#[get("/character/{charcter_id}")]
+async fn get_character(character_id: web::Path<u32>, data: web::Data<Repository>) -> Result<impl Responder> {
+    let character: Option<&Character> = data.characters.get(&character_id);
+
+    match character {
+        Some(character) => Ok(web::Json(character.clone())),
+        None => panic!(), // TODO: Add error handling
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
+    HttpServer::new(|| {
+        App::new()
+            .app_data(web::Data::new(Repository::new()))
+            .service(echo)
+            .service(get_character)
+            .route("/character", web::post().to(create_character))
+        }
+    )
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
